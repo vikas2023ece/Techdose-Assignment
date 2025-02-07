3ee8529f771feb43e91799c2847ea2d406df7476
VIKAS S S <vikas.ss2023ece@sece.ac.in>
2025-02-07 09:44:04 +0530

    Assignment

---
+++ BitManipulation/Assignment/LC169.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       for(int i=0;i<nums.size();i++){
+        int count=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[i]==nums[j]){
+                count++;
+            }
+        }
+      if(count>nums.size()/2){
+        return nums[i];
+      }
+    }
+   return 0;
+    }
+};
